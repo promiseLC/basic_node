3bb7ab14285fc1fbff32ec58997af3cb4eaa8ce7
刘超 <liuchao@aldt.com.cn>
2023-12-18 16:46:30 +0800

    脚本提交

--- child-process/spawn.js
+++ child-process/spawn.js
@@ -2,6 +2,7 @@ import childProcess from 'child_process';
 
 const { spawn, spawnSync } = childProcess
 
+// 个人理解 开启一个子进程 spawn可以执行 命令
 
 const pwd = spawnSync('pwd')
 
@@ -10,3 +11,29 @@ console.log(pwd.stdout.toString());
 const ls = spawnSync('ls', ['-lh'])
 
 console.log(ls.stdout.toString());
+
+
+const fileUrl = 'buffer.js'
+
+// 执行 git log -1 --pretty="%ci" ../fs/index.mjs
+const spawnProcess = spawn('git',['log','-1','--pretty="%ci"',fileUrl])
+
+spawnProcess.stdout.on('data', (data) => {
+  console.log(`stdout${data}`);
+  console.log(new Date(data));
+})
+
+
+const spawnProcessAddAndCommit = () => {
+  
+  const add = spawn('git', ['add', '.'])
+  
+  add.stdout.on('data', (data) => {
+   console.log('add',data);
+  })
+
+   spawn('git',['commit','-m','脚本提交'])
+
+}
+
+spawnProcessAddAndCommit();
